5ebfea8146b59115a39aecda59d1d6766b98c75a
wangbin <c.wangbin@qq.com>
2020-05-23 19:45:09 +0800

    初始化

---
+++ source/other/click-me.js
@@ -0,0 +1,28 @@
+/* 鼠标特效 */
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array("❤除了春天、爱情和樱花❤","❤why❤","❤明明如月❤","❤palette❤","❤rain❤","❤blueming❤","❤离别初体验❤");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        3000,
+        function() {
+            $i.remove();
+        });
+    });
+});
